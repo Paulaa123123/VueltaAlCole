70c32a1b4972dc1fa8017bdb8c0b6771206445c9
Paulaa123123 <84386430+Paulaa123123@users.noreply.github.com>
2024-09-05 10:32:14 +0200

    Revert "Clases Categoria y Producto"
    This reverts commit 58b1ff74a8536c4d82dabec1fb69cb6edbda645b.

--- src/main/java/com/ipartek/modelo/dto/Categoria.java
+++ src/main/java/com/ipartek/modelo/dto/Categoria.java
@@ -1,39 +1,15 @@
 package com.ipartek.modelo.dto;
 
-/**
- * <b>Clase Categoria</b>
- * 
- * <p>Clase Categoria que referencia 
- * la tabla categorias en la BD</p>
- * @author Paula
- *
- */
-
 public class Categoria {
-	/**
-	 * Atributo ID:
-	 * <b>A la hora de insertar en la BD es autonumerico</b>
-	 * 
-	 */
 	private int id;
-	/**
-	 * Atributo nombre:
-	 * <b>varchar(45) en la BD</b>
-	 */
 	private String nombre;
 	
-	/**
-	 * Constructor completo
-	 * @param id un integer, si no lo hemos recogido de la BD deberia ser 0
-	 * @param nombre
-	 */
-
 	public Categoria(int id, String nombre) {
 		super();
 		this.id = id;
 		this.nombre = nombre;
 	}
-
+	
 	public Categoria() {
 		super();
 		this.id = 0;
@@ -60,5 +36,6 @@ public class Categoria {
 	public String toString() {
 		return "Categoria [id=" + id + ", nombre=" + nombre + "]";
 	}
-
+	
+	
 }
